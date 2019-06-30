835d23bf3d05fe8be533dff7632ca4c4f2abbb6f
Joseph DeWitt <jhpdewitt@gmail.com>
2019-06-30 19:35:32 -0400

    Base template with navbar

--- tickets/views.py
+++ tickets/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import Http404
 from django.template import loader
 from .models import Issue
 
 def index(request):
 	issue_list = Issue.objects.order_by('created')
-	template = loader.get_template('tickets/index.html')
 	context = {
 		'issue_list': issue_list,
 	}
-	return HttpResponse(template.render(context, request))
+	return render(request, 'tickets/index.html', context)
 
 def detail(request, issue_id):
-	return HttpResponse("Just one issue, %s." % issue_id)
+	try:
+		issue = Issue.objects.get(pk=issue_id)
+	except Issue.DoesNotExist:
+		raise Http404("This issue does not exist")
+	return render(request, 'tickets/detail.html', {'issue': issue})
 
 def enter_time(request, issue_id):
 	return HttpResponse("You can enter time for %s." % issue_id)
